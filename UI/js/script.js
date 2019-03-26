f090520ad02337295d0f46fbe4c22f66e8b4b24a
fxola <afolabiabass62@gmail.com>
2019-03-26 10:09:46 +0100

    feat(UI): Create sign up page - write html for signup page - write css styles signup page - write html and css for navigation - write javascript file for navigation -  delivers[#164889872]

---
+++ UI/js/script.js
@@ -0,0 +1,7 @@
+let hamburger = document.getElementsByClassName('menu-icon')[0];
+            let list = document.getElementsByTagName('ul')[0];
+
+    hamburger.addEventListener('click', function(){
+                list.classList.toggle('showing');
+            });
+
