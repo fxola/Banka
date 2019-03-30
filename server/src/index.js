fa85436bf1b705c8a76ab47b2c16e8402698cc7f
fxola <afolabiabass62@gmail.com>
2019-03-30 14:39:25 +0100

    chore(server): Setup server and install necessary dependencies - setup eslint and airbnb style guide - setup prettier - install and setup express - install babel, nodemon and other dependencies - setup entry file - setup error handler in entry file - delivers [#165001989]

---
+++ server/src/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  return res.send({
+    status: 200,
+    message: 'Welcome To Banka'
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    status: error.status,
+    message: error.message
+  });
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`server is listening on port:${PORT}`);
+});
+
+export default app;
