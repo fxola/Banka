5fe992aa0f9e3972c3fff55307faf959a74d38b2
fxola <afolabiabass62@gmail.com>
2019-04-01 09:27:35 +0100

    feat(server): Implement User login Functionality - create service and controller methods to handle user login - create and mount login route on user router - delivers[#165013882]

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -40,6 +40,49 @@ class UserService {
       success: true
     };
   }
+
+  /**
+   *
+   * Handles the logic for logging a user into the system
+   * @static
+   * @param {Object} userCredentials present in th incoming request body
+   * @returns {Object} API Response Object
+   * @memberof UserService
+   */
+  static logUserIn(userCredentials) {
+    const { email, password } = userCredentials;
+    const foundUser = mockData.users.find(user => user.email === email);
+    if (!foundUser)
+      return {
+        status: 401,
+        error: 'Authentication Failed. Invalid Login credentials provided',
+        success: false
+      };
+
+    const hash = foundUser.password;
+    if (Helper.comparePassword(password, hash) === true) {
+      const { id, firstName, lastName } = foundUser;
+      const payLoad = { id, firstName, lastName, email };
+      const token = Helper.getToken(payLoad);
+      return {
+        status: 200,
+        data: {
+          token,
+          id,
+          firstName,
+          lastName,
+          email
+        },
+        success: true
+      };
+    }
+
+    return {
+      status: 401,
+      error: 'Authentication Failed. Invalid Login credentials provided',
+      success: false
+    };
+  }
 }
 
 export default UserService;
