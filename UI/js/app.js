e94bf34aa8b2c30d9e0bc5d8f7cb7b0ce73d02a1
fxola <afolabiabass62@gmail.com>
2019-05-07 09:28:43 +0100

    feat(transactions): Consume endpoint for performing debit and credit transactions
    - write logic for consuming debit and credit transactions endpoint
    - update css style for transaction modal
    - add comments to frontend code for clarity
    - delivers[#165829152]

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,28 +1,60 @@
+// base url for all requests on the platform
 const api = "https://bank-a.herokuapp.com/api/v1/";
 
+// lets make our lives easier while trying to query the DOM
 let load = element => document.getElementById(`${element}`);
 let grab = element => document.querySelectorAll(`${element}`);
 
+/**
+ * Handles logic for making Fetch API requests to server
+ *
+ * @class Api
+ * @returns {promise object}
+ */
 class Api {
   static async makeRequest(api, endpoint, options) {
     const response = await fetch(`${api}${endpoint}`, options);
-    console.log(response);
     return await response.json();
   }
 }
 
+/**
+ *  Displays pop-up Notification
+ * @param {object} response
+ */
+const toast = response => {
+  let alertBox = load("alert");
+  alertBox.style.display = "block";
+  alertBox.style.backgroundColor = "red";
+  if (response.success === true) alertBox.style.backgroundColor = "green";
+  alertBox.innerHTML = `<p>${response.message}</p>`;
+  if (!response.message) alertBox.innerHTML = `<p>${response.error}</p>`;
+  setTimeout(() => {
+    alertBox.style.display = "none";
+  }, 3000);
+};
+
+/**
+ * Hnadles Logic for signing a user up on the platform
+ */
 const signUpButton = load("register-button");
 if (signUpButton) {
   signUpButton.addEventListener("click", async e => {
     e.preventDefault();
+
+    // set buuton text to demonstrate initaiation of request
     signUpButton.innerText = `Loading...`;
 
+    // get form values
     const firstName = load("firstname").value;
     const lastName = load("lastname").value;
     const email = load("email").value;
     const password = load("password").value;
     const confirmPassword = load("confirmpassword").value;
+
     const data = { firstName, lastName, email, password, confirmPassword };
+
+    // set fetch API options
     const options = {
       method: "POST",
       headers: {
@@ -34,28 +66,41 @@ if (signUpButton) {
     const response = await Api.makeRequest(api, "auth/signup", options);
 
     if (response.success === false) {
+      // set button text to demonstrate conclusion of request
       signUpButton.innerText = `Register`;
       toast(response);
       return;
     }
+
+    // set button text to demonstrate conclusion of request
     signUpButton.innerText = `Register`;
     toast(response);
+
+    // save token for usage on subsequent requests
     localStorage.setItem("token", response.data.token);
+
+    // redirect to transactions page
     setTimeout(() => {
       window.location.href = "transactions.html";
     }, 3000);
   });
 }
 
+/**
+ * Handles Logic for logging a user(staff/client) into the platform
+ */
 const signInButton = load("login-button");
 if (signInButton) {
   signInButton.addEventListener("click", async e => {
-    e.preventDefault();
+    e.preventDefault(); //prevents page from refreshing on click of the submit button
     signInButton.innerText = `Loading...`;
 
+    // get from values
     const email = load("email").value;
     const password = load("password").value;
     const data = { email, password };
+
+    // set fetch API options
     const options = {
       method: "POST",
       headers: {
@@ -72,8 +117,11 @@ if (signInButton) {
     }
     signInButton.innerText = `Log in`;
     toast(response);
+
+    // save token for usage on sun=bsequent requests
     localStorage.setItem("token", response.data.token);
 
+    // redirect successfully logged in user to appropraite pages based on roles
     switch (response.data.type) {
       case "client":
         setTimeout(() => {
@@ -89,11 +137,16 @@ if (signInButton) {
   });
 }
 
+/**
+ * Handles Logic for displaying all accounts on the platform for a staff user
+ */
 const AccountsSection = load("admin-dashboard-section");
 if (AccountsSection) {
   AccountsSection.innerHTML = `<h1 class="loader">Getting Accounts...</h1>`;
   const token = localStorage.getItem("token");
   if (!token) window.location.href = "index.html";
+
+  // set fetch API options
   const options = {
     method: "GET",
     headers: {
@@ -105,9 +158,13 @@ if (AccountsSection) {
   (async () => {
     try {
       const response = await Api.makeRequest(api, "accounts", options);
+
+      // redirect to hompage if token expires
       if (response.status === 401) window.location.href = "index.html";
 
+      // on event that there are accounts on the platform
       if (response.data.length > 0) {
+        // map all accounts on the platform to adhere to the template provided
         const template = response.data.map(account => {
           const mapped = `
                     <article class="account-details">
@@ -141,35 +198,55 @@ if (AccountsSection) {
           return mapped;
         });
 
+        //attach template to page
         AccountsSection.innerHTML = template;
+
+        // get all 'view accounts' button on page
         const viewAccountButtons = grab("#view-account");
         if (viewAccountButtons) {
+          // attach an event listener to all the view buttons on the page
           for (button of viewAccountButtons) {
             button.addEventListener("click", async e => {
               e.preventDefault();
+              // traverse the DOM  to get the account number of the clicked "view-accounts" button
               const acctNumber =
                 e.target.parentElement.parentElement.parentElement.childNodes[5]
                   .lastElementChild.innerText;
+
+              // save the account number of the clicked account for subsequent usage(making transactions)
               localStorage.setItem("accountnumber", acctNumber);
+
+              // redirect to the template for viewing single account details
               window.location.href = "accounts.html";
             });
           }
         }
         return;
       }
+      // on event that there are no accounts on the platform
       AccountsSection.innerHTML = `<h1 class="loader">${response.message}</h1>`;
     } catch (e) {
+      // Handle unexpected error e.g slow network/server error
       AccountsSection.innerHTML = `<h1 class="loader">Something Went Wrong. Please Try Again</h1>`;
     }
   })();
 }
-
+/**
+ * Handles logic for displaying account information for a single bank account
+ */
 const singleAccount = load("accounts-section");
 if (singleAccount) {
+  // set html content while waiting for response from  the server
   singleAccount.innerHTML = `<h1 class="loader">Fetching Account Details...<h1>`;
+
+  // get request parameters
   const acctNumber = localStorage.getItem("accountnumber");
   const token = localStorage.getItem("token");
+
+  // redirect to hompage if token isn't available
   if (!token) window.location.href = "index.html";
+
+  // set fetch API options
   const options = {
     method: "GET",
     headers: {
@@ -185,10 +262,13 @@ if (singleAccount) {
         `accounts/${acctNumber}`,
         options
       );
-      if (response.status === 401) window.location.href = "index.html";
-      const userAccountDetails = response.data;
 
+      // redirect to hompage if token expires
+      if (response.status === 401) window.location.href = "index.html";
+
+      const userAccountDetails = response.data;
       const template = `<article class="account-record">
+                      <article id="alert"></article>
                       <article class="record-details">
                         <figure>
                           <img
@@ -230,33 +310,72 @@ if (singleAccount) {
                               <article class="transaction-modal-content" id="transaction-modal">
                                   <form action="#" id="transaction-form">
                                       <article class="fields">
-                                              <input  id="transaction-type" list="type" placeholder="Select Transaction Type (credit or debit)">
-                                              <datalist id="type">
-                                                      <option>credit</option>
-                                                      <option>debit</option>
-                                              </datalist>
-                                              <input type="amount"  placeholder="Transaction Amount">
+                                              <select required id="transaction-type"  placeholder="Select Transaction Type (credit or debit)">
+                                                      <option value = "credit">credit</option>
+                                                      <option value = "debit">debit</option>
+                                              </select>
+                                              <input type="number"  id ="amount"placeholder="Transaction Amount" required>
                                       </article>
-                                          <a href="#"><button class="confirm" >Make Transaction</button><a/>
+                                          <a href="#"><button class="confirm" id="confirm">Make Transaction</button><a/>
                                           <a href="#"><button class="cancel" >Cancel</button><a/>
                                       </form>
                               </article>
                     </article>`;
+
+      // set template on getting succesful response from server
       singleAccount.innerHTML = template;
+
+      /**
+       * Handles Logic for performing credit/debit transactions
+       */
+      const transactionForm = load("transaction-form");
+      if (transactionForm) {
+        const transactionButton = load("confirm");
+        if (transactionButton) {
+          transactionButton.addEventListener("click", async e => {
+            e.preventDefault(); //prevents page from refreshing on click of the submit button
+
+            // get form values
+            const transactionType = load("transaction-type").value;
+            const amount = load("amount").value;
+
+            //get request parameters
+            const token = localStorage.getItem("token");
+            const acctNumber = localStorage.getItem("accountnumber");
+
+            const data = { amount };
+            const options = {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+              },
+              body: JSON.stringify(data)
+            };
+
+            // make Api call
+            try {
+              const response = await Api.makeRequest(
+                api,
+                `transactions/${acctNumber}/${transactionType}`,
+                options
+              );
+              window.location.href = "#";
+              toast(response);
+            } catch (e) {
+              // Handle unexpected error e.g slow network/server error, while making transaction
+              window.location.href = "#";
+              toast({
+                success: false,
+                message: `Something Went Wrong. Please Try Again`
+              });
+            }
+          });
+        }
+      }
     } catch (e) {
+      // Handle unexpected error e.g slow network/server error, while geeting account information
       singleAccount.innerHTML = `<h1 class="loader">Something Went Wrong. Please Try Again</h1>`;
     }
   })();
 }
-
-const toast = response => {
-  let alertBox = load("alert");
-  alertBox.style.display = "block";
-  alertBox.style.backgroundColor = "red";
-  if (response.success === true) alertBox.style.backgroundColor = "green";
-  alertBox.innerHTML = `<p>${response.message}</p>`;
-  if (!response.message) alertBox.innerHTML = `<p>${response.error}</p>`;
-  setTimeout(() => {
-    alertBox.style.display = "none";
-  }, 2000);
-};
